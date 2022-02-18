e76e04183a78f15b828915a60b65ce0e16b56e79
David Mandolini <damandolini@gmail.com>
2022-02-17 21:11:05 -0600

    Add routes and votes methods

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,17 @@
 class VotesController < ApplicationController
+  def create
+    vote = Vote.new(
+      :user_id => params[:user_id],
+      :nomination_id => params[:nomination_id],
+    )
+    vote.save
+    render json: vote
+  end
+
+  def update
+    vote = Vote.find_by(:id => params[:id])
+    vote.nomination_id = params[:nomination_id]
+    vote.save
+    render json: vote
+  end
 end
