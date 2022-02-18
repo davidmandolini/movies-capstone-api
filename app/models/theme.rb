c2fd19ecdb46c29638d51e7cb28fc41e25345f7b
David Mandolini <damandolini@gmail.com>
2022-02-17 19:54:00 -0600

    complete seeds file

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,3 @@
 class Theme < ApplicationRecord
   has_many :events
-  belongs_to :user, optional: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   has_many :nominations
-  has_many :themes
   has_many :user_groups
   has_many :groups, through: :user_groups
   has_many :votes
