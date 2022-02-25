883bc8c06dc17fced95cb2b46e2dafab1a725b74
David Mandolini <damandolini@gmail.com>
2022-02-24 18:55:50 -0600

    Add event and group methods

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,11 @@
 class GroupsController < ApplicationController
+  def index
+    groups = Group.all
+    render json: groups
+  end
+
+  def show
+    group = Group.find_by(:id => params[:id])
+    render json: group
+  end
 end
