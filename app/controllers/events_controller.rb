bbd235a3b5a38bd5779206650fa8010b5f8e51b4
David Mandolini <damandolini@gmail.com>
2022-02-24 19:52:29 -0600

    Add nominations create method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,2 @@
 class EventsController < ApplicationController
-  # def index
-  #   events = Event.all
-  #   render json: events
-  # end
-
-  # def show
-  #   event = Event.find_by(:id => params[:id])
-  #   render json: event
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,15 +9,11 @@ Rails.application.routes.draw do
   # patch "/groups/:id" => "groups#update"
   # delete "/groups/:id" => "groups#destroy"
 
-  # get "/events" => "events#index"
   # post "/events" => "events#create"
-  # get "/events/:id" => "events#show"
   # patch "/events/:id" => "events#update"
   # delete "/events/:id" => "events#destroy"
 
-  # get "/nominations" => "nominations#index"
-  # post "/nominations" => "nominations#create"
-  # get "/nominations/:id" => "nominations#show"
+  post "/nominations" => "nominations#create"
   # patch "/nominations/:id" => "nominations#update"
   # delete "/nominations/:id" => "nominations#destroy"
 
