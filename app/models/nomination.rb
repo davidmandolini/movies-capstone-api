4a3c0ff8dbb811746b36c7318afa92fd1b4e51e4
David Mandolini <damandolini@gmail.com>
2022-03-17 20:46:28 -0500

    Add youtube_id to nominations

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -2,4 +2,11 @@ class Nomination < ApplicationRecord
   belongs_to :event
   belongs_to :user
   has_many :votes
+
+  def youtube_id
+    if trailer_url
+      id = trailer_url.delete_prefix("https://www.youtube.com/watch?v=")
+      return id
+    end
+  end
 end
--- app/serializers/nomination_serializer.rb
+++ app/serializers/nomination_serializer.rb
@@ -1,3 +1,3 @@
 class NominationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :year, :rating, :runtime, :genre, :director, :plot, :imdb_rating, :metacritic_rating, :poster, :votes, :event_id, :trailer_url
+  attributes :id, :name, :year, :rating, :runtime, :genre, :director, :plot, :imdb_rating, :metacritic_rating, :poster, :votes, :event_id, :trailer_url, :youtube_id
 end
